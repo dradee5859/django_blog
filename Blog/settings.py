cb0d4f7f29e2eacbcee02afd27ecb0ed517a6885
dradee5859 <109266465+dradee5859@users.noreply.github.com>
2022-09-29 11:06:38 -0700

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
