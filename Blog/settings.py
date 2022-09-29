20a987d20f61c76a681605f81b3392aaeb539ced
dradee5859 <109266465+dradee5859@users.noreply.github.com>
2022-09-29 11:02:06 -0700

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
